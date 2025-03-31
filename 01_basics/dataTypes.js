f6af8f95fdd7db9c77c313bfb620344e0b2285c7
VarshitAgrawal <65728500+VarshitAgrawal@users.noreply.github.com>
2025-03-31 18:45:01 +0000

    js learning

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,2 @@
+// their are 2 types of data types -- premitive type and non premitive 
+// "use strict"; used to treat all the js code with newer version
---
+++ 01_basics/var.js
@@ -0,0 +1,7 @@
+var emailId = "24"
+let emailAddress = "var@gmail.com"
+const pwd = "abc";
+phnNo = '8484851'
+
+emailAddress='@gmail.com'
+console.table([emailId,emailAddress,pwd,phnNo]);
