405d4df80f1494ac562e8089c0a30ddadb83e1d9
VarshitAgrawal <65728500+VarshitAgrawal@users.noreply.github.com>
2025-04-13 10:40:51 +0000

    data types

--- 01_basics/dataTypes.js
+++ 01_basics/dataTypes.js
@@ -1,2 +1,51 @@
 // their are 2 types of data types -- premitive type and non premitive 
 // "use strict"; used to treat all the js code with newer version
+let a=10
+console.log(typeof a)
+let num=10.5
+console.log(typeof num)
+
+a="hello"
+console.log(typeof a)
+
+let msg='Good morning'
+console.log(typeof msg)
+
+let islogin=true
+console.log(typeof islogin)
+
+let x
+console.log(typeof x)//undefined
+x="hi everyone!!"
+console.log(typeof x)
+
+let y=null
+console.log(typeof y)
+
+let nums=[10,20,30,40,50]
+
+console.log(typeof nums)
+
+let user={
+    name:"jhon",
+    age:40,
+    loc:"india"
+}
+
+console.log(typeof user)
+"use strict";//treat all JS code for newer version
+// types of datatypes: Number, string,boolean,object,undefined
+
+// Primitive Data types==>
+// number=> 2 to power 53
+// bigint 
+//string=>""
+//boolean=> true,false
+// null ==>it is standalone value which means empty or unkwnown value , type of null is object
+// undefined ==> variable is declared but value is not assigned
+// symbol => react, figma,for unique it is used
+
+
+//object
+
+console.log(typeof '');
