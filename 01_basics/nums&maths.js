460984fd03e1c8b7b088df3805f5f74a6a05effd
VarshitAgrawal <65728500+VarshitAgrawal@users.noreply.github.com>
2026-03-17 02:37:54 +0530

    Update nums&maths.js

--- 01_basics/nums&maths.js
+++ 01_basics/nums&maths.js
@@ -22,8 +22,8 @@ console.log(num.toLocaleString('en-IN'));// 2,00,00,00,000 will convert accordin
 
 // Maths Lib ==>
   console.log(Math);
-  console.log(Math.ceil(5.6));// here ceil meaning top floor so output willbe 5
-  console.log(Math.floor(8.5)); // here o/p will be floor so 9
+  console.log(Math.ceil(5.6));// here ceil meaning top floor so output willbe 6
+  console.log(Math.floor(8.5)); // here o/p will be floor so 8
   console.log(Math.round(4.6));
   console.log(Math.abs(-5));// use to convert - to plus
     
